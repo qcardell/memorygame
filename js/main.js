acf64629522ddb9b83b7459b9049eef9f9f4de9e
Quinnton Cato <qcardell@live.com>
2018-09-08 09:37:07 -0500

    Add winning screen and text

--- js/main.js
+++ js/main.js
@@ -7,6 +7,43 @@ var cardstocompare = new Array();
 var flipboolean = false;
 var numberofMatches = 0;
 
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+//var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+//btn.onclick = function() {
+//    modal.style.display = "block";
+//}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function openmodal()
+{
+    var modalText=document.querySelectorAll('.modaltext');
+    console.log(modalText);
+    modalText[0].textContent = "Congratulations! You Won!";
+    modalText[1].textContent = "With "+movesCount + " Moves and 1 Stars";
+    modalText[2].textContent = "Woooooooo!";
+    modal.style.display = "block";
+}
+
 function turnCompatible(elem, src) {
     $(elem).animate({
         width: 0,
@@ -52,7 +89,7 @@ $(".turnCSS").click(function () {
     //console.log("this = "+this.src);
     //console.log("back = "+back);
     //selects images based on ID number
-
+    openmodal();
     if(this.textContent != 'flipped'){
         //this.addClass("face");
     switch(this.id){
